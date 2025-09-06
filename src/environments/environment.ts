225b0a05b723b5ce7ae97912e164b08ae8f43cd5
unknown <sebastian.vegar2015@gmail.com>
2025-09-06 09:38:00 -0500

    add files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000', // URL local para desarrollo
+  apiUrl: 'https://music-uptc-sogamoso-back.vercel.app',
   firebaseConfig: {
-    apiKey: 'your-api-key',
-    authDomain: 'your-project.firebaseapp.com',
-    projectId: 'your-project-id',
-    storageBucket: 'your-project.appspot.com',
-    messagingSenderId: '123456789',
-    appId: 'your-app-id',
+    apiKey: 'AIzaSyAehdTsKYuyXdmBngoGthTpaMPrw4nsga4',
+    authDomain: 'musicuptcsogamoso.firebaseapp.com',
+    projectId: 'musicuptcsogamoso',
+    storageBucket: 'musicuptcsogamoso.firebasestorage.app',
+    messagingSenderId: '1011403818121',
+    appId: '1:1011403818121:web:eea45507f1bdcd3631b258',
+    measurementId: 'G-BFW1T6D1J1',
   },
 };
