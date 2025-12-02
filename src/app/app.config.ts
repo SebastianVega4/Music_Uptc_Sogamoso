bdca47ec5220f2f2ee4f2425407558898665ed71
unknown <sebastian.vegar2015@gmail.com>
2025-12-02 15:12:03 -0500

    add menu

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,30 +1,12 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; // ← NECESARIO
-import { provideClientHydration } from '@angular/platform-browser'; // ← RECOMENDADO
-
-// Firebase - NECESARIOS
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-
 import { routes } from './app.routes';
-import { environment } from '../environments/environment'; // ← Asegúrate de tener esto
-
-
+import { provideHttpClient } from '@angular/common/http'; 
+import { provideClientHydration } from '@angular/platform-browser';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(), // ← Para HttpClient
-
+    provideHttpClient(),
     provideClientHydration(),
-    // Firebase providers
-
-    // Firebase providers
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-      provideAuth(() => getAuth()),
-      provideFirestore(() => getFirestore())
-    )
   ]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,4 @@ export const environment = {
   apiUrl: 'http://localhost:5000',
   supabaseUrl: 'https://nkojabtbosvzbfcveyfm.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rb2phYnRib3N2emJmY3ZleWZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNjgwNzAsImV4cCI6MjA3Mjk0NDA3MH0.rJdadnVoVBw-vrt2XCm3619YzSIX3jSaxRftJQdAw00',
-  firebaseConfig: {
-    apiKey: 'AIzaSyAehdTsKYuyXdmBngoGthTpaMPrw4nsga4',
-    authDomain: 'musicuptcsogamoso.firebaseapp.com',
-    projectId: 'musicuptcsogamoso',
-    storageBucket: 'musicuptcsogamoso.firebasestorage.app',
-    messagingSenderId: '1011403818121',
-    appId: '1:1011403818121:web:eea45507f1bdcd3631b258',
-    measurementId: 'G-BFW1T6D1J1',
-  },
 };
